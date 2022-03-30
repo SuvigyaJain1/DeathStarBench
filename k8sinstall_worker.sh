ebf09aa8f1508017836f89d6102b6c81baeb5a8b
Suvigya Jain <suvigyajain1@gmail.com>
2022-03-30 17:30:29 +0530

    fixed worker script

--- k8sinstall_worker.sh
+++ k8sinstall_worker.sh
@@ -11,7 +11,8 @@ else
     sudo apt-get update && apt-get upgrade -y
     sudo apt-get install docker.io
     sudo apt-get install -y apt-transport-https ca-certificates curl
-    sudo apt-get install sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+    sudo apt-get install
+    sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
     echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
     sudo apt-get update
     sudo apt-get install -y kubelet kubeadm kubectl
