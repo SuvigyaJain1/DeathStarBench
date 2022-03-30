59ac15b024bac8a4d5a86c2f53390092ac2ff238
= <=>
2022-03-30 07:09:12 -0700

    Update workload generator

--- socialNetwork/wrk2/workload_generator.py
+++ socialNetwork/wrk2/workload_generator.py
@@ -1,14 +1,15 @@
 import subprocess
+import os
+import time
 
 # Address for the master node
-NGINX_URL = "http://192.168.191.197:8080"
-OUTPUT_DIR = "./outputs/"
+NGINX_URL = "http://165.232.188.194:8080"
+OUTPUT_DIR = "./outputs"
 
 def run(dist, n_thread, n_conns, dur_sec, script, req_per_sec, filename):
-        cmd = f"./wrk -D {dist} -t {n_thread} -c {n_conns} -d {dur_sec}s -L\
-        -s{script} {NGINX_URL}/wrk2-api/post/compose -R {req_per_sec} > {OUTPUT_DIR}/{filename}.txt"
-        subprocess.run(cmd)
-
+        cmd = f"./wrk -D {dist} -t {n_thread} -c {n_conns} -d {dur_sec}s -L -s {script} {NGINX_URL}/wrk2-api/post/compose -R {req_per_sec} > {OUTPUT_DIR}/{filename}.txt"
+        os.system(cmd)
+        time.sleep(int(dur_sec)+10)
 
 def main():
     for i in [1, 10, 20, 50, 100, 200, 400, 600, 800, 1000, 10000]:
