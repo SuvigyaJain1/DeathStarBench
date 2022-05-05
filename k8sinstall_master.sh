191c2b1bccdbfbd3c0b81c3942f141a4b6a94904
supreethkurpad <supreethkurpad@gmail.com>
2022-05-05 12:05:28 +0000

    updated install script

--- k8sinstall_master.sh
+++ k8sinstall_master.sh
@@ -23,19 +23,44 @@ else
         sudo apt-get install -y kubelet kubeadm kubectl
         sudo apt-mark hold kubelet kubeadm kubectl
 
-        ## Changing C-group driver
-        # su -c "touch /etc/docker/daemon.json"
-        # su -c "echo '{ 
-        # "exec-opts": ["native.cgroupdriver=systemd"]
-        # }'| sudo cat > /etc/docker/daemon.json"
-        # sudo systemctl daemon-reload
-        # sudo systemctl restart docker
-        # sudo systemctl restart kubelet
+        sudo apt-get remove docker docker-engine docker.io containerd runc
+        sudo apt-get install gnupg lsb-release
+        curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+        echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+        $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-        sudo usermod -aG docker $USER
+        sudo apt-get update
+        sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin  
+        cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay 
+br_netfilter
+EOF
 
-        sudo kubeadm init --apiserver-advertise-address $NODE_IP --pod-network-cidr 192.168.0.0/16
+        sudo modprobe overlay
+        sudo modprobe br_netfilter
 
+        # Setup required sysctl params, these persist across reboots.
+        cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+        # Apply sysctl params without reboot
+        sudo sysctl --system
+        sudo mkdir -p /etc/containerd
+        containerd config default | sudo tee /etc/containerd/config.toml
+        sudo systemctl restart containerd
+
+        # To use the systemd cgroup driver in /etc/containerd/config.toml with runc, set
+        # [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc]
+        # ...
+        # [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc.options]
+        #     SystemdCgroup = true
+
+        sudo kubeadm init --config kubeadm-config.yaml
+        
         mkdir -p $HOME/.kube
         sudo cp  /etc/kubernetes/admin.conf $HOME/.kube/config
         sudo chown $(id -u):$(id -g) $HOME/.kube/config
