12a1e8cbcf5d06ea5ce5f5d2c220a81259c60842
supreethkurpad <supreethkurpad@gmail.com>
2022-05-05 04:45:11 +0000

    update install script

--- k8sinstall_master.sh
+++ k8sinstall_master.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ $# -ne 2 ];
+if [ $# -ne 1 ];
 then
-    echo "Expected Usage: $0 <Node's public IP Address> <Pod-Network CIDR Address>"
+    echo "Expected Usage: $0 <Node's public IP Address> "
 else
     if [ $USER = "root" ];
     then
@@ -12,33 +12,32 @@ else
         echo "sudo su - <username>"
     else
 
-        MASTER_IP=$1
-        POD_NET=$2
+        NODE_IP=$1
 
         sudo apt-get update && apt-get upgrade -y
         sudo apt-get install docker.io
-        sudo apt-get install -y apt-transport-https ca-certificates curl
-        sudo apt-get install sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+        sudo apt-get install -y apt-transport-https ca-certificates curl 
+        sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
         echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
         sudo apt-get update
         sudo apt-get install -y kubelet kubeadm kubectl
-        sudo apt-mark hold kubelet kubeadm kubectl
+#        sudo apt-mark hold kubelet kubeadm kubectl
 
         # Changing C-group driver
-        touch /etc/docker/daemon.json
-        echo '{ 
+        su -c "touch /etc/docker/daemon.json"
+        su -c "echo '{ 
         "exec-opts": ["native.cgroupdriver=systemd"]
-        }'| cat > /etc/docker/daemon.json
+        }'| sudo cat > /etc/docker/daemon.json"
         sudo systemctl daemon-reload
         sudo systemctl restart docker
         sudo systemctl restart kubelet
 
         sudo usermod -aG docker $USER
 
-        kubeadm init --apiserver-advertise-address $MASTER_IP --pod-network-cidr $POD_NET
+        sudo kubeadm init --apiserver-advertise-address $NODE_IP --pod-network-cidr 192.168.0.0/16
 
         mkdir -p $HOME/.kube
-        sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+        sudo cp  /etc/kubernetes/admin.conf $HOME/.kube/config
         sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
         kubectl create -f https://projectcalico.docs.tigera.io/manifests/tigera-operator.yaml
