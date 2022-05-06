b70f451f02e75b435332a98e203dcf2f44bfd38f
= <=>
2022-05-06 08:12:39 -0700

    update workload gen

--- socialNetwork/wrk2/workload_generator.py
+++ socialNetwork/wrk2/workload_generator.py
@@ -3,8 +3,8 @@ import os
 import time
 
 # Address for the master node
-NGINX_URL = "http://165.232.188.194:31111/"
-OUTPUT_DIR = "./outputs"
+NGINX_URL = "http://128.199.25.250:31111/"
+OUTPUT_DIR = "./client-external-without-istio"
 
 def run(dist, n_thread, n_conns, dur_sec, script, req_per_sec, filename):
         cmd = f"./wrk -D {dist} -t {n_thread} -c {n_conns} -d {dur_sec}s -L -s {script} {NGINX_URL}/wrk2-api/post/compose -R {req_per_sec} > {OUTPUT_DIR}/{filename}.txt"
@@ -12,8 +12,8 @@ def run(dist, n_thread, n_conns, dur_sec, script, req_per_sec, filename):
         time.sleep(int(dur_sec)+10)
 
 def main():
-    for i in [1, 10, 20, 50, 100, 200, 400, 600, 800, 1000, 10000]:
-        run("norm", 2, 500, 100,  "./scripts/social-network/compose-post.lua", i, f"rps_{i}")
+    for i in [20,50,100,200,500,1000]:
+        run("fixed", 2, 100, 60,  "./scripts/social-network/compose-post.lua", i, f"rps_{i}")
 
 if __name__ == "__main__":
     main()
