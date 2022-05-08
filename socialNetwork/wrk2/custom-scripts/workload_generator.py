5ecf795a99e8a433335356c33eb120a6a5a17b5b
= <=>
2022-05-08 03:04:34 -0700

    add latest outputs

--- socialNetwork/wrk2/custom-scripts/workload_generator.py
+++ socialNetwork/wrk2/custom-scripts/workload_generator.py
@@ -7,9 +7,9 @@ NGINX_URL = "http://128.199.25.250:31111"
 OUTPUT_DIR = "./outputs"
 
 def run(dist, n_thread, n_conns, dur_sec, script, req_per_sec, filename):
-        cmd = f"wrk -D {dist} -t {n_thread} -c {n_conns} -d {dur_sec}s -L -s {script} {NGINX_URL}/wrk2-api/home-timeline/read -R {req_per_sec} > {OUTPUT_DIR}/{filename}.txt"
+        cmd = f"wrk -D {dist} -t {n_thread} -c {n_conns} -d {dur_sec}s -L -s {script} {NGINX_URL}/wrk2-api/home-timeline/read -R {req_per_sec} > {OUTPUT_DIR}/{filename}"
         os.system(cmd)
-        time.sleep(int(dur_sec)+10)
+
 
 def main():
     for i in [20,50,100,200,500,1000]:
