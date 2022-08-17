1d6ec2f7a19277bb9c964f9470915b76893b4f0a
Supreeth G Kurpad <supreethkurpad@gmail.com>
2022-08-17 18:32:52 +0530

    Create auto-profiler.py

---
+++ socialNetwork/wrk2/auto-profiler.py
@@ -0,0 +1,87 @@
+import subprocess
+import os
+import time
+import csv
+
+# Address for the master node
+NGINX_URL = "http://128.199.25.250:31111"
+OUTPUT_DIR = "./outputs"
+
+TO_STRING = ["Distribution", "Threads", "Connections(s)", "Duration", "Script", "Endpoint", "Requests Per Second"]
+PARAMS_LIST = [
+    ["fixed", 2, 100, 60,  "/home/surpeeth/DeathStarBench/socialNetwork/wrk2/scripts/social-network/read-home-timeline.lua", "wrk2-api/post/compose", 10],
+    ["fixed", 2, 100, 60,  "/home/surpeeth/DeathStarBench/socialNetwork/wrk2/scripts/social-network/read-home-timeline.lua", "wrk2-api/read-timeline/home", 10]
+]
+
+OBSERVABLES = [2,3]
+HEADERS = ['Run ID', 'Command Number'] + [TO_STRING[i] for i in OBSERVABLES] + ['Mean', 'StdDeviation', 'Max', 'Total count', 'Buckets', 'SubBuckets']
+NUM_RUNS = 2
+
+FOLDER_ID = 0
+FILE_ID = 0
+
+rows = []
+
+def parse(run_id, data, line):
+    line = line[2:-2]
+    stats = line.split(',')
+
+    for stat in stats:
+        key, value = stat.split('=')
+        key, value = key.strip(), value.strip()
+        data[key] = float(value)
+
+def parse_file(run_id, data, file):
+    with open (file) as f:
+        for line in f:
+            if line[0] == '#':
+                parse(run_id, data, line)
+
+def run(dist, n_thread, n_conns, dur_sec, script, endpoint, req_per_sec, path):
+        cmd = f"wrk -D {dist} -t {n_thread} -c {n_conns} -d {dur_sec}s -L -s {script} {NGINX_URL}/{endpoint} -R {req_per_sec} > {path}"
+        os.system(cmd)
+        # os.system(f"cp ./wrk-output.txt {path}")
+        # print(cmd)
+
+def get_runid():
+    global FILE_ID
+    FILE_ID += 1
+    return str(FILE_ID)
+
+def get_path(folder, filename):
+    return f"{OUTPUT_DIR}/{folder}/{filename}"
+
+def get_folder():
+    global FOLDER_ID
+    FOLDER_ID += 1
+    folder = f"COMMAND_{FOLDER_ID}"
+    os.system(f"mkdir -p {OUTPUT_DIR}/{folder}")
+    return folder
+
+def populate_observables(run_id, data, params):
+    for factor in OBSERVABLES:
+        data[TO_STRING[factor]] = params[factor]
+
+def main():
+    for params in PARAMS_LIST:
+        dist, n_thread, n_conns, dur_sec, script, endpoint, rps = params
+        folder = get_folder()
+        for i in range(NUM_RUNS):
+            data = {}
+            run_id = get_runid()
+            data['Run ID'] = run_id
+            data['Command Number'] = FOLDER_ID
+            populate_observables(run_id, data, params)
+            path = get_path(folder, run_id)
+            run(dist, n_thread, n_conns, dur_sec, script, endpoint, rps, path)
+            parse_file(run_id, data, path)
+            rows.append(data)
+
+if __name__ == "__main__":
+    main()
+    # print(headers)
+    # print(rows)
+    with open(f'./{OUTPUT_DIR}/output.csv', 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames = HEADERS)
+        writer.writeheader()
+        writer.writerows(rows)
