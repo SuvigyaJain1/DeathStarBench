7c8cae05d4ea4c17d2c9985c041a2fc5f6a2c6f0
Suvigya Jain <suvigyajain1@gmail.com>
2022-09-11 22:13:53 +0530

    update scale-test

--- socialNetwork/wrk2/scale-test.sh
+++ socialNetwork/wrk2/scale-test.sh
@@ -4,7 +4,8 @@ for n in 1 2 3 4 5
 do
     kubectl scale --all --replicas=$n --namespace=default deployment
     sleep 300
-    ./wrk -D fixed -t 8 -c 16 -d 60 -L -s ./scripts/social-network/compose-post.lua http://localhost:8080/wrk2-api/post/compose -R 1000 > $OUTPUT_DIR/($n)_replicas.txt
+    ./wrk -D fixed -t 8 -c 16 -d 60 -L -s ./scripts/social-network/compose-post.lua http://localhost:8080/wrk2-api/post/compose -R 1000 > $OUTPUT_DIR/compose/($n)_replicas.txt
+    ./wrk -D fixed -t 8 -c 16 -d 60 -L -s ./scripts/social-network/read-user-timeline.lua http://localhost:8080/wrk2-api/user-timeline/read -R 1000 > $OUTPUT_DIR/read/($n)_replicas.txt
     echo "Finished run with $n replicas"
     uptime
 done
