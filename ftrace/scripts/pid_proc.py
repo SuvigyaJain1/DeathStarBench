940954f11cf61a1bcc16cc61ec6446394e956ed2
Supreeth G Kurpad <supreethkurpad@gmail.com>
2022-11-01 14:38:07 +0530

    Delete pid_proc.py

--- ftrace/scripts/pid_proc.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-import subprocess
-
-trace_file = sys.argv[1]
-pid = sys.argv[2]
-
-output = subprocess.getoutput(f"cat {trace_file} | grep {pid}")
-print(''.join(output.split("\n")[0].split()[1].split("-")[:-1]))
