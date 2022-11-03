2f704bbfb75c7675124a4e1b3ef427a383a056ec
SuvigyaJain1 <suvigyajain1@gmail.com>
2022-11-03 10:55:58 +0530

    fixed parser to work with nested callgraphs

--- ftrace/scripts/parser.py
+++ ftrace/scripts/parser.py
@@ -11,10 +11,25 @@ class Line:
     def __init__(self, line):
         line = line.strip()
         self.is_log_line = self.log_line(line)
+        self.level = -1
+
         if not self.is_log_line:
             return
         
-        self.process, self.time, self.function_name = [value.strip() for value in line.split("|")]
+        self.process, self.time, self.function_name = [value for value in line.split("|")]
+        self.process = self.process.strip()
+        self.time = self.time.strip()
+
+        # Finding the indentation level of the function call
+        function_name = self.function_name.strip()
+        self.level = self.function_name.index(function_name) // 2
+        self.function_name = function_name
+        self.opening_bracket = "{" in self.function_name
+        self.closing_bracket = "}" in self.function_name
+
+        if self.closing_bracket:
+            self.level = -1
+        
         *procname, self.pid = self.process.split("-")
 
         self.time = self.time.strip()
@@ -25,8 +40,6 @@ class Line:
                 print(self.time)
             # self.time, unit = self.time.split(" ")
         
-        self.opening_bracket = "{" in self.function_name
-        self.closing_bracket = "}" in self.function_name
 
         # if self.opening_bracket:
         self.function_name = self.function_name.strip("{")
@@ -51,13 +64,24 @@ class Node:
             self.open_brackets = 0
     
     def add_child_to_node(self, line):
+
         if line.closing_bracket:
-            self.open_brackets -= 1
-            self.time = line.time
-        else: 
+            self.open_brackets -= 1           
+            if self.open_brackets == 0:
+                self.time = line.time
+            elif self.open_brackets > 0:
+                self.children[-1].add_child_to_node(line)
+            else:
+                print("Bracket count < 0! Error")
+        else:
+            if self.children and self.children[-1].open_brackets:
+                self.children[-1].add_child_to_node(line)
+            else:
+                self.children.append(Node(line))
+
             if line.opening_bracket:
                 self.open_brackets += 1
-            self.children.append(Node(line))
+     
     
     def to_dict(self):
         # return {"pid": self.pid, "time": self.time, }
@@ -67,10 +91,11 @@ class Node:
         d["children"] = [i.to_dict() for i in self.children]
         return d
 
+
 def main():
     trace_folder = 'WITHOUT_ISTIO_WITH_WORKLOAD'
     num_runs=3
-    os.makedirs('parsedOutputs/'+trace_folder)
+    os.makedirs('parsedOutputs/'+trace_folder, exist_ok=True)
 
     for i in range(num_runs):
         trace_file = open(trace_folder+'/'+str(i)+'.txt', 'r')
@@ -83,15 +108,22 @@ def main():
             if pid not in result.keys():
                 result[pid] = [node]
             else:
-                if not result[pid][-1].open_brackets == 0:
+                if result[pid][-1].open_brackets:
                     result[pid][-1].add_child_to_node(line)
                 else:
                     result[pid].append(node)
+        
+        CLEARED_START = False
 
         for line in trace_file:
             line = Line(line)
+            if not CLEARED_START and line.level != 1:
+                continue
+            
+            CLEARED_START = True
             if not line.is_log_line:
                 continue
+
             add_node_to_result(line)
 
 
