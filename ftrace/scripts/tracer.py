68f3be5c0a3a38b658387eaa16324507883239f9
suvigyajain1@gmail.com <suvigyajain1@gmail.com>
2022-11-10 04:08:03 +0000

    new tracer sets cmdlnes and store wrk

--- ftrace/scripts/tracer.py
+++ ftrace/scripts/tracer.py
@@ -65,7 +65,11 @@ def set_graph_depth(depth):
 
 def backup_trace(destination_path):
     trace_path = f"{FTRACE_HOME}/trace"
-    output(f"cp {trace_path} {destination_path}")
+    saved_cmdlines_path = f"{FTRACE_HOME}/saved_cmdlines"
+
+    output(f"cp {trace_path} {destination_path}.txt")
+    output(f"cp {saved_cmdlines_path} {destination_path}.saved_cmdlines")
+
     with open(f"{destination_path}.pid.pickle", 'wb') as f:
         pickle.dump(PID_MAP, f)
 
@@ -77,10 +81,15 @@ def set_options(options):
     file_path = f"{FTRACE_HOME}/trace_options"
     append_to_file(file_path, options)
 
+def set_saved_cmdlines_size(size):
+    file_path = f"{FTRACE_HOME}/saved_cmdlines_size"
+    write_to_file(file_path, size)
+
 def setup_ftrace(processes):
     turn_tracing_off()
     set_current_tracer("function_graph")
     set_graph_depth("0")
+    set_saved_cmdlines_size(10000)
     clear_trace_log()
     clear_ftrace_pid_filter()
     set_options("userstacktrace")
@@ -88,8 +97,16 @@ def setup_ftrace(processes):
     set_options("funcgraph-duration")
     set_options("display-graph")
     set_options("funcgraph-proc")
+    set_options("function-fork")
     for process in processes:
         add_ftrace_pid_filter(process)
+
+def workload_trigger(wrk_command):
+    wrk_output = output(wrk_command)
+    return wrk_output
+
+def clear_os_cache():
+    return output("sync; echo 3 > /proc/sys/vm/drop_caches")
 
 def main(CONFIG):
     '''
@@ -110,35 +127,46 @@ def main(CONFIG):
     workload_command = CONFIG["workload_command"]
     trace_delay = CONFIG["trace_delay"]
 
-
     output(f"mkdir {name}")
     setup_ftrace(processes)
 
     for trace_number in range(num_traces):
         
         clear_trace_log()
-        if with_workload:
-            async_workload_trigger(workload_command)
-        
-        time.sleep(trace_delay)
+        clear_os_cache()
+
         turn_tracing_on()
-        time.sleep(trace_time)
+        time.sleep(trace_delay)
+
+        if with_workload:
+            wrk_output = workload_trigger(workload_command)
+            write_to_file(f"{name}/{trace_number}.wrk", wrk_output)
+        else:
+            time.sleep(trace_time)
+        
         turn_tracing_off()
 
-        output_file = f"{name}/{trace_number}.txt"
+        output_file = f"{name}/{trace_number}"
         backup_trace(output_file)
-        time.sleep(WORKLOAD_DURATION - trace_time - trace_delay)
+
+        time.sleep(1)
 
 if __name__ == '__main__':
     CONFIG = {}
+    
+    # Workloads are now blocking -> Either wrk command or the trace time is used at once.
+        # If wrk command is used, the script collects traces only for the duration of the workload and trace_time has no effect.
+        # else, it sleeps for trace_time seconds and collects traces
+        # In both cases, trace_delay is added.
+        # tracing delay is run after turning on trace and before running workload/trace sleep.
 
-    WORKLOAD_DURATION = 100
-    CONFIG["name"] = "WITH_ISTIO_WITH_WORKLOAD"
-    CONFIG["num_traces"] = 3
+    WORKLOAD_DURATION = 60 # Used only if with_workloads = True
+    CONFIG["name"] = "WITHOUT_ISTIO_WITHOUT_WORKLOAD"
+    CONFIG["num_traces"] = 5
     CONFIG["processes"] = ['memcached','redis','Service','mongo', 'nginx','proxy','pilot']
-    CONFIG["trace_time"] = 60
-    CONFIG["with_workload"] = True
+    CONFIG["trace_time"] = 60 # Used only if with_workloads = False
+    CONFIG["with_workload"] = False
     CONFIG["workload_duration"] = WORKLOAD_DURATION
     CONFIG["workload_command"] = f'../../socialNetwork/wrk2/wrk -D fixed -t 1 -c 1 -d {WORKLOAD_DURATION}s -L -s ../../socialNetwork/wrk2/scripts/social-network/compose-post.lua http://localhost:31111/wrk2-api/post/compose -R 100'
-    CONFIG["trace_delay"] = 30
+    CONFIG["trace_delay"] = 1
     main(CONFIG)
